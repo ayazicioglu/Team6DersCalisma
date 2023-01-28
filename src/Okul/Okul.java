e8c53827345b2aecdb4ad0e8754b9320e04c3813
ayazicioglu <aahmetyazicioglu@gmail.com>
2023-01-28 15:05:31 +0300

    28.01.23

--- src/Okul/Okul.java
+++ src/Okul/Okul.java
@@ -1,2 +1,20 @@
-package Okul;public class Okul {
+package Okul;
+
+
+public class Okul {
+    static final String okulAdi="Yıldız Koleji";
+    static final String adres="Ankara";
+    static final String telefon="31211111111";
+    private Okul() {
+    }
+
+    public static void okulBilgileriniGoruntule() {
+        System.out.println("\nOkul Adı: "+okulAdi +"\nAdres: "+adres+"\nTelefon: "+telefon);
+
+    }
+
+    @Override
+    public String toString() {
+        return "Okul{}";
+    }
 }
